62bb7aaa70cc56d0655c337e49b7e7ecf3bdd723
4zeddine <azeddine.nader@gmail.com>
2024-05-24 12:25:12 -0700

    edit all

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,92 +1,70 @@
 #!/usr/bin/python3
-""" objects that handle all default RestFul API actions for States """
-from models.state import State
-from models import storage
+"""State view objects that handles all default RESTFul API actions
+"""
+
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
+from flask import abort, jsonify, request
+from models import storage
+from models.state import State
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get_state.yml', methods=['GET'])
-def get_states():
+@app_views.route('/states', strict_slashes=False)
+def states():
+    """returns a list of all states
     """
-    Retrieves the list of all State objects
+    return jsonify([state.to_dict() for state in storage.all(State).values()])
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False)
+def state(state_id):
+    """returns a state object by id
     """
-    all_states = storage.all(State).values()
-    list_states = []
-    for state in all_states:
-        list_states.append(state.to_dict())
-    return jsonify(list_states)
-
-
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get_id_state.yml', methods=['get'])
-def get_state(state_id):
-    """ Retrieves a specific State """
     state = storage.get(State, state_id)
-    if not state:
+    if state is None:
         abort(404)
+    return state.to_dict()
 
-    return jsonify(state.to_dict())
 
-
-@app_views.route('/states/<state_id>', methods=['DELETE'],
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'],
                  strict_slashes=False)
-@swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
 def delete_state(state_id):
+    """deletes a state object by id
     """
-    Deletes a State Object
-    """
-
     state = storage.get(State, state_id)
-
-    if not state:
+    if state is None:
         abort(404)
-
-    storage.delete(state)
+    state.delete()
     storage.save()
-
-    return make_response(jsonify({}), 200)
+    return {}, 200
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-@swag_from('documentation/state/post_state.yml', methods=['POST'])
-def post_state():
+def create_state():
+    """creates a state object
     """
-    Creates a State
-    """
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-
-    if 'name' not in request.get_json():
-        abort(400, description="Missing name")
-
-    data = request.get_json()
-    instance = State(**data)
-    instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+    state = request.get_json()
+    if state is None:
+        abort(400, "Not a JSON")
+    if "name" not in state:
+        abort(400, "Missing name")
+    state = State(**state)
+    state.save()
+    return state.to_dict(), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/state/put_state.yml', methods=['PUT'])
-def put_state(state_id):
-    """
-    Updates a State
+def update_state(state_id):
+    """updates a state object
     """
     state = storage.get(State, state_id)
-
-    if not state:
+    if state is None:
         abort(404)
-
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-
-    ignore = ['id', 'created_at', 'updated_at']
-
-    data = request.get_json()
-    for key, value in data.items():
-        if key not in ignore:
+    update = request.get_json()
+    if update is None:
+        abort(400, "Not a JSON")
+    for key, value in update.items():
+        if key not in ["id", "created_at", "updated_at"]:
             setattr(state, key, value)
-    storage.save()
-    return make_response(jsonify(state.to_dict()), 200)
+    state.save()
+    return state.to_dict(), 200
