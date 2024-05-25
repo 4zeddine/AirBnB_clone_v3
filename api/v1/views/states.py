eda96d59a55e9b5e8736f20318b51f13c1eb7612
essaket <hichamessaket17@gmail.com>
2024-05-25 10:33:03 -0700

    Add by essaket

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,70 +1,72 @@
 #!/usr/bin/python3
-"""State view objects that handles all default RESTFul API actions
-"""
-
+"""states"""
 from api.v1.views import app_views
-from flask import abort, jsonify, request
+from flask import jsonify, abort, request
 from models import storage
 from models.state import State
+from datetime import datetime
+import uuid
 
 
-@app_views.route('/states', strict_slashes=False)
-def states():
-    """returns a list of all states
-    """
-    return jsonify([state.to_dict() for state in storage.all(State).values()])
+@app_views.route('/states/', methods=['GET'])
+def list_states():
+    '''Retrieves a list of all State objects'''
+    list_states = [obj.to_dict() for obj in storage.all("State").values()]
+    return jsonify(list_states)
 
 
-@app_views.route('/states/<state_id>', strict_slashes=False)
-def state(state_id):
-    """returns a state object by id
-    """
-    state = storage.get(State, state_id)
-    if state is None:
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    '''Retrieves a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
         abort(404)
-    return state.to_dict()
+    return jsonify(state_obj[0])
 
 
-@app_views.route('/states/<state_id>',
-                 methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
-    """deletes a state object by id
-    """
-    state = storage.get(State, state_id)
-    if state is None:
+    '''Deletes a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
         abort(404)
-    state.delete()
-    storage.save()
-    return {}, 200
+    state_obj.remove(state_obj[0])
+    for obj in all_states:
+        if obj.id == state_id:
+            storage.delete(obj)
+            storage.save()
+    return jsonify({}), 200
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/', methods=['POST'])
 def create_state():
-    """creates a state object
-    """
-    state = request.get_json()
-    if state is None:
-        abort(400, "Not a JSON")
-    if "name" not in state:
-        abort(400, "Missing name")
-    state = State(**state)
-    state.save()
-    return state.to_dict(), 201
+    '''Creates a State'''
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    states = []
+    new_state = State(name=request.json['name'])
+    storage.new(new_state)
+    storage.save()
+    states.append(new_state.to_dict())
+    return jsonify(states[0]), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def update_state(state_id):
-    """updates a state object
-    """
-    state = storage.get(State, state_id)
-    if state is None:
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def updates_state(state_id):
+    '''Updates a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
         abort(404)
-    update = request.get_json()
-    if update is None:
-        abort(400, "Not a JSON")
-    for key, value in update.items():
-        if key not in ["id", "created_at", "updated_at"]:
-            setattr(state, key, value)
-    state.save()
-    return state.to_dict(), 200
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    state_obj[0]['name'] = request.json['name']
+    for obj in all_states:
+        if obj.id == state_id:
+            obj.name = request.json['name']
+    storage.save()
+    return jsonify(state_obj[0]), 200
